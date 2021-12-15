4c6cfe393db40f6d050eed86a2a3427dc9255054
RajanKunwar <75790060+RajanKunwar@users.noreply.github.com>
2021-12-15 18:16:20 +0545

    Create Abstract.java

---
+++ LAb_Practicals/lab1/Abstract.java
@@ -0,0 +1,15 @@
+AbstractClass.java
+abstract class AbstractClass{
+	abstract void help();
+}
+
+AbsDemo.java
+class AbsDemo extends AbstractClass{
+	public void help(){
+	System.out.println("Abclass methode overide....");
+	}
+	public static void main(String[] args) {
+		AbstractClass abClass = new AbsDemo();
+		abClass.help();
+	}
+}
